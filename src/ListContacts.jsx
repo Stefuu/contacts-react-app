307dce5b266d449da8e7624d7c551ae94e11f1ea
Stefano Damiano <stefano.damiano@totvs.com.br>
2018-02-25 22:27:09 +0000

    transforming contactList in a class component

--- src/ListContacts.jsx
+++ src/ListContacts.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-let ListContacts = ({contacts, removeContact}) => (
-    <ol className='contact-list'>
-        {contacts.map(contact => (
-            <li className='contact-list-item' key={contact.id}>
-                <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }} />
-                <div className='contact-details'>
-                    <p>{contact.name}</p>
-                    <p>{contact.email}</p>
-                </div>
-                <button onClick={() => removeContact(contact)} className='contact-remove'>Remove</button>
-            </li>
-        ))}
-    </ol>
-)
+class ListContacts extends React.Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        removeContact: PropTypes.func.isRequired
+    }
 
-ListContacts.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    removeContact: PropTypes.func.isRequired
+    render(){
+        return (
+            <ol className='contact-list'>
+                {this.props.contacts.map(contact => (
+                    <li className='contact-list-item' key={contact.id}>
+                        <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }} />
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                        <button onClick={() => this.props.removeContact(contact)} className='contact-remove'>Remove</button>
+                    </li>
+                ))}
+            </ol>
+        )
+    }
 }
 
 export default ListContacts
